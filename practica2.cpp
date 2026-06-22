269dd73de0053cffd741e869c57bf769c9341f19
marchenko274 <marchenko24@kmt.in.ua>
2026-06-22 16:27:44 +0300

    Add files via upload

---
+++ practica2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int day, month, year;
+
+    cout << "Введіть день: ";
+    cin >> day;
+
+    cout << "Введіть місяць: ";
+    cin >> month;
+
+    cout << "Введіть рік: ";
+    cin >> year;
+
+    if (month < 3)
+    {
+        month += 12;
+        year--;
+    }
+
+    int K = year % 100;
+    int J = year / 100;
+
+    int h = (day + (13 * (month + 1)) / 5 + K + K / 4 + J / 4 + 5 * J) % 7;
+
+    string days[] =
+    {
+        "субота",
+        "неділя",
+        "понеділок",
+        "вівторок",
+        "середа",
+        "четвер",
+        "п'ятниця"
+    };
+
+    cout << "\nДень тижня: " << days[h] << endl;
+
+    system("pause");
+    return 0;
+}
