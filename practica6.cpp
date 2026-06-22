a826aa4e7feb91f465a5c293ff84f186b90bbd40
marchenko274 <marchenko24@kmt.in.ua>
2026-06-22 17:01:37 +0300

    Update practica6.cpp

--- practica6.cpp
+++ practica6.cpp
@@ -1,64 +1,120 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 #include <iomanip>
 
 using namespace std;
 
-double taylorSin(double x, double eps)
+const double PI = 3.14159265358979323846;
+
+typedef struct
 {
-    double term = x;
-    double sum = x;
-    int n = 1;
+    double re;
+    double im;
+} complex_num;
 
-    while (fabs(term) > eps)
-    {
-        term *= -x * x / ((2 * n) * (2 * n + 1));
-        sum += term;
-        n++;
-    }
+complex_num divideComplex(complex_num z1, complex_num z2)
+{
+    complex_num result;
 
-    return sum;
+    double denominator =
+        z2.re * z2.re +
+        z2.im * z2.im;
+
+    result.re =
+        (z1.re * z2.re +
+         z1.im * z2.im) / denominator;
+
+    result.im =
+        (z1.im * z2.re -
+         z1.re * z2.im) / denominator;
+
+    return result;
+}
+
+void printComplex(complex_num z)
+{
+    cout << fixed << setprecision(6);
+
+    cout << z.re;
+
+    if (z.im >= 0)
+        cout << " + i*" << z.im;
+    else
+        cout << " - i*" << fabs(z.im);
 }
 
 int main()
 {
-    double x1, x2, dx, eps;
+    double R, L, C;
+    double fmin, fmax, df;
 
-    cout << "x1 = ";
-    cin >> x1;
+    cout << "Enter R (Ohm): ";
+    cin >> R;
 
-    cout << "x2 = ";
-    cin >> x2;
+    cout << "Enter L (mH): ";
+    cin >> L;
 
-    cout << "dx = ";
-    cin >> dx;
+    cout << "Enter C (uF): ";
+    cin >> C;
 
-    cout << "epsilon = ";
-    cin >> eps;
+    cout << "Enter fmin: ";
+    cin >> fmin;
+
+    cout << "Enter fmax: ";
+    cin >> fmax;
+
+    cout << "Enter step df: ";
+    cin >> df;
+
+    if (R <= 0 || L <= 0 || C <= 0)
+    {
+        cout << "Error: all parameters must be positive!" << endl;
+        return 1;
+    }
+
+    if (fmin > fmax)
+    {
+        cout << "Error: fmin must be less than or equal to fmax!" << endl;
+        return 1;
+    }
+
+    // Перевод единиц измерения
+    L = L / 1000.0;      // mH -> H
+    C = C / 1000000.0;   // uF -> F
+
+    double f0 =
+        1.0 /
+        (2.0 * PI * sqrt(L * C));
 
     cout << fixed << setprecision(6);
 
-    cout << "\n"
-        << setw(10) << "x"
-        << setw(15) << "Taylor"
-        << setw(15) << "sin(x)"
-        << setw(15) << "Difference"
-        << endl;
+    cout << "\nResonance frequency f0 = "
+         << f0 << " Hz\n";
 
-    for (double x = x1; x <= x2; x += dx)
+    cout << "\nFrequency\tZ\n";
+
+    for (double f = fmin; f <= fmax; f += df)
     {
-       const double PI = 3.14159265358979323846;
-double rad = x * PI / 180.0;
+        double w = 2.0 * PI * f;
 
-        double taylor = taylorSin(rad, eps);
-        double standard = sin(rad);
+        complex_num numerator;
+        numerator.re = L / C;
+        numerator.im = R / (w * C);
 
-        cout << setw(10) << x
-            << setw(15) << taylor
-            << setw(15) << standard
-            << setw(15) << fabs(taylor - standard)
-            << endl;
+        complex_num denominator;
+        denominator.re = R;
+        denominator.im =
+            w * L -
+            1.0 / (w * C);
+
+        complex_num Z =
+            divideComplex(numerator, denominator);
+
+        cout << "f = " << f << "\tZ = ";
+        printComplex(Z);
+        cout << endl;
     }
 
+    system("pause");
     return 0;
 }
